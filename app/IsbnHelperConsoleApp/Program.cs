3965bed260348a04dec632529fbffa196589442a
workshop-1c71e191-b6a2-4a53-8cf0-4fea15eb651b <user@codecommit.aws>
2023-11-14 11:38:27 -0800

    Add IsbnHelper

---
+++ app/IsbnHelperConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+string ISBN = null;
+do
+{
+    var isbnHelper = new IsbnExtensions.IsbnSearch.IsbnSearch();
+   
+    Console.WriteLine("Please enter a 10 digit ISBN:");
+    ISBN = Console.ReadLine();
+
+    if (string.IsNullOrEmpty(ISBN))
+    {
+        break;
+    }
+    
+    var result = await isbnHelper.GetIsbnResults(ISBN);
+    Console.WriteLine($"For ISBN {ISBN}:");
+    Console.WriteLine($"NumPages:\t{result.NumberOfPages})");
+    Console.WriteLine($"PubDate:\t{result.PublishDate}");
+    Console.WriteLine($"Publishers:");
+    result.Publishers.ForEach(Console.WriteLine);
+
+} while (true);
+Console.Write("Bye!!");
