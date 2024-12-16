66c58a50fba31e9bf4ae31d6eaefd49fe96d8b40
NoReply Amazon <no-reply@amazon.com>
2024-12-16 19:24:34 +0000

    [Commit] Iteration 20 #errors = 6 <= 8.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/app/Bookstore.Web/Controllers/AuthenticationController.cs][CS1061][27,46] 'IResponseCookies' does not contain a definition for 'Add' and no accessible extension method 'Add' accepting a first argument of type 'IResponseCookies' could be found (are you missing a using directive or an assembly reference?)``.

--- app/Bookstore.Web/Controllers/AuthenticationController.cs
+++ app/Bookstore.Web/Controllers/AuthenticationController.cs
@@ -1,7 +1,7 @@
 using System;
-using System.Web;
-using BobsBookstoreClassic.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using BobsBookstoreClassic.Data;
 
 
 namespace Bookstore.Web.Controllers
@@ -24,7 +24,8 @@ namespace Bookstore.Web.Controllers
         {
             if (HttpContext.Request.Cookies["LocalAuthentication"] != null)
             {
-                HttpContext.Response.Cookies.Add(new HttpCookie("LocalAuthentication") { Expires = DateTime.Now.AddDays(-1) });
+                HttpContext.Response.Cookies.Delete("LocalAuthentication");
+                HttpContext.Response.Cookies.Append("LocalAuthentication", "", new CookieOptions { Expires = DateTime.Now.AddDays(-1) });
             }
 
             return RedirectToAction("Index", "Home");
@@ -34,7 +35,8 @@ namespace Bookstore.Web.Controllers
         {
             if (Request.Cookies[".AspNet.Cookies"] != null)
             {
-                Response.Cookies.Add(new HttpCookie(".AspNet.Cookies") { Expires = DateTime.Now.AddDays(-1) });
+                Response.Cookies.Delete(".AspNet.Cookies");
+                Response.Cookies.Append(".AspNet.Cookies", "", new CookieOptions { Expires = DateTime.Now.AddDays(-1) });
             }
 
             var domain = BookstoreConfiguration.Get("Authentication/Cognito/CognitoDomain");
